60ebdbc8aca5af1cbc4805b23115280f6693ff90
myin <minyin142p@gmail.com>
2023-05-14 19:01:51 +0200

    remove some steps for build

--- scripts/ci/build-channel.sh
+++ scripts/ci/build-channel.sh
@@ -2,7 +2,7 @@
 
 CHANNEL=${CHANNEL:-$1}
 
-mkdir ~/.config
-cp /root/.config/godot ~/.config -r
+# mkdir ~/.config
+# cp /root/.config/godot ~/.config -r
 mkdir -v -p build/$CHANNEL
 godot -v --export $CHANNEL
