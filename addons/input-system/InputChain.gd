e10f0448a3a6996b014bbc1fb7617db248a61204
myin <minyin142p@gmail.com>
2023-05-14 10:48:21 +0200

    add input chain

---
+++ addons/input-system/InputChain.gd
@@ -0,0 +1,73 @@
+class_name InputChain
+extends Node
+
+var valid_inputs = []
+var chain = ChainNode.new("")
+var active_chain = null
+
+func handle_input(ev: InputEvent):
+	if not _is_valid_input(ev):
+		print("invalid input: %s" % ev.as_text())
+		return
+	
+	var c = active_chain if active_chain != null else chain
+	active_chain = c.find_for_event(ev)
+	print("Pressed %s chained to %s" % [ev.as_text(), active_chain])
+
+func _is_valid_input(ev: InputEvent):
+	for i in valid_inputs:
+		if ev.is_action_pressed(i):
+			return true
+	return false
+
+func add_chain(inputs: Array[String], action):
+	var curr = chain
+	for input in inputs:
+		if not input in valid_inputs:
+			valid_inputs.append(input)
+			
+		var nextChain = curr.find_child(input)
+		if nextChain:
+			curr = nextChain
+			continue
+		
+		var newChain = ChainNode.new(input)
+		curr.children.append(newChain)
+		curr = newChain
+		
+	curr.action = action
+
+func get_chain_action():
+	if active_chain == null:
+		return null
+	
+	print("Active chain: %s" % active_chain)
+	var action = active_chain.action
+	active_chain = null
+	return action
+
+class ChainNode:
+	var children: Array[ChainNode] = []
+	var key: String
+	var action
+	
+	func _init(key):
+		self.key = key
+
+	func find_for_event(ev: InputEvent):
+		for child in children:
+			if ev.is_action(child.key):
+				return child
+		return null
+
+	func find_child(input: String):
+		for child in children:
+			if child.key == input:
+				return child
+		return null
+		
+	func _to_string():
+		var c = []
+		for child in children:
+			c.append(child.key)
+		return "%s: [%s]" % [key, ", ".join(c)]
--- src/player/player.gd
+++ src/player/player.gd
@@ -1,5 +1,17 @@
 extends CharacterBody2D
 
+const U = "move_up"
+const D = "move_down"
+const L = "move_left"
+const R = "move_right"
+
+enum Action {
+	THUNDERSTORM,
+	FIREBALL,
+	EARTHQUAKE,
+	TELEPORT,
+}
+
 signal died()
 
 @export var speed := 100
@@ -8,36 +20,59 @@ signal died()
 @export var input: PlayerInput
 @export var anim: SpriteAnimTool
 @export var hand: Hand
+@export var cast_circle: Sprite2D
 
 @export var cursor_root: Node2D
 @export var sprite: Sprite2D
 @export var spell_caster: SpellCaster
+@export var chain: InputChain
+
+var casting = false
 
 func _ready():
-	anim.start_play("move")
+	chain.add_chain([U, D, U, L, R], Action.THUNDERSTORM)
+	chain.add_chain([L, R, R, U, L], Action.FIREBALL)
+	chain.add_chain([D, L, R, D, U], Action.EARTHQUAKE)
+	chain.add_chain([R, L], Action.TELEPORT)
 
 func _process(delta):
 	var dir = global_position.direction_to(get_global_mouse_position())
 	cursor_root.rotation = dir.angle()
 	
+	casting = input.is_pressed("cast")
 	sprite.flip_h = dir.x < 0
 
 func _physics_process(delta):
-	var motion = Vector2(
-		input.get_action_strength("move_right") - input.get_action_strength("move_left"),
-		input.get_action_strength("move_down") - input.get_action_strength("move_up")
-	)
-	
-	velocity = velocity.move_toward(motion * speed, accel * delta)
-	move_and_slide()
+	if casting:
+		anim.start_play("idle")
+		cast_circle.visible = true
+		velocity = Vector2.ZERO
+	else:
+		anim.start_play("move")
+		cast_circle.visible = false
+		var motion = Vector2(
+			input.get_action_strength("move_right") - input.get_action_strength("move_left"),
+			input.get_action_strength("move_down") - input.get_action_strength("move_up")
+		)
+		
+		velocity = velocity.move_toward(motion * speed, accel * delta)
+		move_and_slide()
 
 
 func _on_player_input_just_pressed(ev: InputEvent):
-	if ev.is_action("fire"):
-		spell_caster.fire()
-	elif ev.is_action("interact"):
-		hand.interact()
+	if not casting:
+		if ev.is_action("fire"):
+			spell_caster.fire()
+		elif ev.is_action("interact"):
+			hand.interact()
+	else:
+		chain.handle_input(ev)
 
+func _on_player_input_just_released(ev: InputEvent):
+	if ev.is_action("cast"):
+		var action = chain.get_chain_action()
+		if action != null:
+			print("Action: %s" % Action.keys()[action])
 
 func _on_health_died():
 	input.disable()
