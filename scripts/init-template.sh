27ddb30fd2d09eccdebe4ec7d0cde76e442eb45a
myin <minyin142p@gmail.com>
2023-05-18 17:04:42 +0200

    feat: create publish script

--- scripts/init-template.sh
+++ scripts/init-template.sh
@@ -8,3 +8,4 @@ fi
 echo "Setting game name to $GAME_NAME"
 sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" project.godot
 sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" .github/workflows/release.yml
+sed -i "s/##VAR_GAME_NAME/$GAME_NAME/g" scripts/publish.sh
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,71 @@
 #!/bin/sh
 
-# Run in case the CI does not work properly
+# Deps:
+# generate-changelog
+# github-cli
+# butler
+
+GAME="lost-library"
+VERSION=$1
+if [[ -z $VERSION ]]; then
+   echo "Version missing"
+   exit 0
+fi
+
+ARGS=("$@")
+if [[ ${#ARGS[@]} == 1 ]]; then
+    echo "No channels"
+    exit 0
+fi
+shift
+
 CHANNELS=( "$@" )
 
-for CHANNEL in "${CHANNELS[@]}"
-do
-   ./scripts/ci/build-channel.sh "$CHANNEL"
-   butler push build/$CHANNEL kuma-gee/lost-library:$CHANNEL
-done
+generate_changelog() {
+    touch package.json
+    echo "{}" > package.json
+    changelog -p -t $VERSION -f CHANGELOG.md
+    rm package.json
+}
+
+build_channels() {
+    for CHANNEL in "${CHANNELS[@]}"; do
+        ./scripts/build-channel.sh $CHANNEL
+    done
+}
+
+github_release() {
+    echo "Releasing version $VERSION to github for $CHANNELS"
+
+    cd build
+    mkdir -p gh-releases
+    for CHANNEL in "${CHANNELS[@]}"; do
+        echo "Archiving $CHANNEL"
+        cp $CHANNEL $GAME -r
+        zip -r gh-releases/$GAME-$CHANNEL.zip $GAME
+        rm $GAME -rf
+    done
+
+    echo "Creating release"
+    gh release create $VERSION gh-releases/* -n "" -t ""
+    rm gh-releases -rf
+}
+
+itch_release() {
+    echo "Releasing version $VERSION to itch.io for $CHANNELS"
+
+    for CHANNEL in "${CHANNELS[@]}"; do
+        echo "Releasing $CHANNEL"
+        butler push build/$CHANNEL kuma-gee/$GAME:$CHANNEL --userversion $VERSION
+    done
+}
+
+generate_changelog
+
+# build_channels
+
+# if [[ $VERSION != *"-rc"* ]]; then
+#     itch_release
+# fi
+
+# github_release
