10ebbf95d9b94a06785f89e162c51f9cdb4b6646
myin <minyin142p@gmail.com>
2023-05-20 19:37:35 +0200

    fix: start menu particles not emitting

--- src/ui/start_menu.gd
+++ src/ui/start_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @export var open_door_sound: AudioStreamPlayer
+@export var particles: GPUParticles2D
+
+func _ready():
+	particles.emitting = true
 
 func _on_start_pressed():
 	open_door_sound.play()
