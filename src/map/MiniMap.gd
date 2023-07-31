cc0eca5594c160309dd1ae6368d7adbae1ccab51
myin <min.yin@evolit.com>
2023-07-31 15:22:46 +0200

    minimap camera

--- src/map/MiniMap.gd
+++ src/map/MiniMap.gd
@@ -1,6 +1,9 @@
 class_name MiniMap
 extends TileMap
 
+@export var accel := 200
+@export var camera: Camera2D
+@export var main_camera: Camera2D
 @export var tileMap: DungeonMap
 
 const SOURCE = 1
@@ -8,6 +11,8 @@ const ATLAS = Vector2(13, 8)
 
 const SOURCE_LAYER = 0
 const TARGET_LAYER = 0
+
+@onready var camera_scale := Vector2(tile_set.tile_size) / Vector2(tileMap.tile_set.tile_size)
 
 func _ready():
 	tileMap.tiles_updated.connect(_update)
@@ -19,3 +24,7 @@ func _update():
 		var data = tileMap.get_cell_tile_data(SOURCE_LAYER, cell)
 		if DungeonMap.is_ground_type(data):
 			set_cell(TARGET_LAYER, cell, SOURCE, ATLAS)
+
+func _process(delta):
+	var dest = main_camera.global_position * Vector2(camera_scale)
+	camera.global_position = dest #camera.global_position.move_toward(dest, accel * delta)
